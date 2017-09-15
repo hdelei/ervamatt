95e36d0ca83ddc40e6ae49d1bb1e1c5c0787ed78
hdelei <hdelei@gmail.com>
2017-09-15 23:12:52 +0000

    Site ervamatt funcional com agenda e História dinamicos

--- site1/models.py
+++ site1/models.py
@@ -3,37 +3,43 @@ from django.utils import timezone
 
 class Historia(models.Model):
     #author = models.ForeignKey('auth.User')
-    titulo = models.CharField(max_length=200)
-    texto = models.TextField()
-    
+    titulo = models.CharField(max_length=200,
+            verbose_name="Título do cabeçalho")
+    texto = models.TextField(verbose_name="Hitória da banda")
+
+
     def __str__(self):
         return self.titulo
-		
+
+
+    class Meta:
+        verbose_name = 'História'
+        verbose_name_plural = 'História'
+
+
 class Agenda(models.Model):
     #author = models.ForeignKey('auth.User')
-    local = models.CharField(max_length=200)
-    endereco = models.CharField(max_length=200)
-    data = models.DateTimeField(
-            default=timezone.now)
-    img = models.ImageField(upload_to = "site1/static/img_schedule",
-			default = '/static/img_schedule/sched.png',
-			blank=True)
-	#img_name = models.CharField(img.url.replace)
+    local = models.CharField(max_length=200,
+            verbose_name='Local do Evento')
+    endereco = models.CharField(max_length=200,
+            verbose_name='Endereço')
+    data = models.DateTimeField(default=timezone.now,
+            verbose_name='Data do evento')
+    img = models.ImageField(upload_to = 'ervamatt/static/img_schedule',
+			default = 'ervamatt/static/img_schedule/sched.png',
+			blank=True,
+			verbose_name="Foto ou logotipo do local")
+
+
+    def get_name(self):
+        img_name = self.img.url.replace('ervamatt/', '')
+        return img_name
+
 
     def __str__(self):
         return self.local
-	
-	#def is_active(self):
-		
 
-'''
-class Schedule(models.Model):
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    show_date = models.DateTimeField(
-            default=timezone.now)
-    img = models.ImageField(upload_to = "static/img_schedule",blank=True)
 
-    def __str__(self):
-        return self.title
-'''
+    class Meta:
+        verbose_name = 'Agenda de shows'
+        verbose_name_plural = 'Agenda de shows'
--- site1/views.py
+++ site1/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
-from django.utils import timezone
 from .models import Historia
 from .models import Agenda
 
 def post_list(request):
 	historia = Historia.objects.first()
 	agenda = Agenda.objects.all()
-	return render(request, 'site1/post_list.html', 
-		{'historia':historia, 
+	return render(request, 'site1/post_list.html',
+		{'historia':historia,
 		'agenda':agenda})
-		
+
 def main_site(request):
 	historia = Historia.objects.first()
 	agenda = Agenda.objects.all()
-	return render(request, 'site1/index.html', 
-		{'historia':historia, 
+	return render(request, 'site1/index.html',
+		{'historia':historia,
 		'agenda':agenda})
